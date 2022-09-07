cc5b3b90a5cdc22c6486af5e097e27109009e419
kngansokewt <tranngan55599@gmail.com>
2022-09-08 00:15:35 +0700

    kim ngan

---
+++ app/src/main/java/com/example/button_switch_activity/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.button_switch_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button backActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        backActivity = findViewById(R.id.button2);
+        backActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
